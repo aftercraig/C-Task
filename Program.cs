d664d0d023256ddcc717a0c4f2ca01f8bf1b6558
aftercraig <pavelboritko@gmail.com>
2023-10-25 01:21:15 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[,] moss = new int[10, 10];
+
+Random ran = new Random();
+
+for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; i++) {
+        moss[i, j] = ran.Next(0, 0);
+        Console.Write("0\t", moss[i, j]);
+    }
+    Console.WriteLine();
+}
